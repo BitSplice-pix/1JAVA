c7ca4bfce50150730a8cc9bc691e0ac3ae454f82
BitSplice <salmanansari74070@gmail.com>
2025-08-14 20:15:14 +0530

    new line

--- firstprogram.java
+++ firstprogram.java
@@ -1,5 +1,9 @@
+
 public class firstprogram {
+
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("This is a new line 1 ");
+        System.err.println("This is a new line 2 ");
     }
 }
